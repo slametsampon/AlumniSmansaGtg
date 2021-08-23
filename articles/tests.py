2e6b0ef616679f5c4e0bbe697f4fbbddf1cec5ab
slametsampon <slamet@pon.co.id>
2021-08-23 12:49:06 +0700

    work on test

--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,59 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse, resolve
+from .models import Article, ArticleComment
 
-# Create your tests here.
+class ArticleTest(TestCase):
+    def test_create_article(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username = 'will', 
+            email = 'will@email.com', 
+            address = 'maron', 
+            mobile_number = '0811-3323-706', 
+            bio = 'short bio', 
+            password = 'test123')
+        
+        article = Article.objects.create(
+            title = 'kancil nyolong timun',
+            author = user
+        )
+        
+        self.assertEqual(article.title,'kancil nyolong timun')
+        self.assertEqual(article.author,user)
+    
+    
+class ArticleCommentTest(TestCase):
+    def test_create_article_comment(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username = 'will', 
+            email = 'will@email.com', 
+            address = 'maron', 
+            mobile_number = '0811-3323-706', 
+            bio = 'short bio', 
+            password = 'test123')
+        
+        article = Article.objects.create(
+            title = 'kancil nyolong timun',
+            author = user
+        )
+        
+        articleComment = ArticleComment.objects.create(
+            description = "It's nice ",
+            author = user,
+            article = article
+        )
+        
+        self.assertEqual(articleComment.description,"It's nice ")
+        self.assertEqual(articleComment.article,article)
+        self.assertEqual(articleComment.author,user)
+    
+class ArticleCreateTest(TestCase):
+    def setUp(self):
+        url = reverse('articles:articleCreate')
+        self.response = self.client.get(url)
+        
+    def test_articlecreate_template(self):
+        pass
+        
--- pages/tests.py
+++ pages/tests.py
@@ -1,23 +1,17 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse, resolve # new
 from .views import HomePageView, AboutPageView # new
 
-class HomepageTests(SimpleTestCase):
+class HomepageTests(TestCase):
 
     def setUp(self):
         url = reverse('home')
         self.response = self.client.get(url)
 
-    def test_homepage_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
-
     def test_homepage_template(self):
+        self.assertEqual(self.response.status_code, 200)
         self.assertTemplateUsed(self.response, 'home.html')
-
-    def test_homepage_contains_correct_html(self):
         self.assertContains(self.response, 'Pulang Kampung')
-
-    def test_homepage_does_not_contain_incorrect_html(self):
         self.assertNotContains(
             self.response, 'Hi there! I should not be on the page.')
 
@@ -28,22 +22,16 @@ class HomepageTests(SimpleTestCase):
             HomePageView.as_view().__name__
         )
 
-class AboutPageTests(SimpleTestCase): # new
+class AboutPageTests(TestCase): # new
 
     def setUp(self):
         url = reverse('about')
         self.response = self.client.get(url)
 
-    def test_aboutpage_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
-
     def test_aboutpage_template(self):
+        self.assertEqual(self.response.status_code, 200)
         self.assertTemplateUsed(self.response, 'about.html')
-
-    def test_aboutpage_contains_correct_html(self):
         self.assertContains(self.response, 'About Page')
-
-    def test_aboutpage_does_not_contain_incorrect_html(self):
         self.assertNotContains(
             self.response, 'Hi there! I should not be on the page.')
 
